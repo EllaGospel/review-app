49ab44dd780e321f02d69b6d922590aa982eb922
ViktoreNnamdi <nnamdyvictor@gmail.com>
2023-03-01 15:41:36 +0100

    calculated average and total review

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./component/Header"
+import ReviewStats from "./component/ReviewStats"
 import ReviewList from "./component/ReviewList"
 import './index.css'
 import {useState} from 'react'
@@ -6,14 +7,17 @@ import ReviewData from "./component/ReviewData"
 
 function App() {
   const [review, setReview] = useState(ReviewData)
-
-
-
+  const deleteReview =(id) =>{
+    if(window.confirm('Are you sure you want to delete this item?')){
+      setReview(review.filter((item)=>item.id!==id))
+    }
+  }
 
   return (
     <div className="container">
       <Header/>
-      <ReviewList review={review}/>
+      <ReviewStats review={review}/>
+      <ReviewList review={review} revDelete={deleteReview}/>
       
       
     </div>
