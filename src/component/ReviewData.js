f13166043af0314351baf2ae2c1daf512618104f
ViktoreNnamdi <nnamdyvictor@gmail.com>
2023-03-12 19:45:25 +0100

    only one data

--- src/component/ReviewData.js
+++ src/component/ReviewData.js
@@ -1,20 +1,20 @@
 const ReviewData = [
-    {
-        id:1,
-        rating:4,
-        text:'Services require some improvement'
-      },
-      {
-        id:2,
-        rating:7,
-        text:'I am please with yor services'
-      },
+    // {
+    //     id:1,
+    //     rating:4,
+    //     text:'Services require some improvement'
+    //   },
+    //   {
+    //     id:2,
+    //     rating:7,
+    //     text:'I am please with yor services'
+    //   },
   
-      {
-        id:3,
-        rating:9,
-        text:'Your services are top notch'
-      },
+    //   {
+    //     id:3,
+    //     rating:9,
+    //     text:'Your services are top notch'
+    //   },
       {
         id:4,
         rating:8,
