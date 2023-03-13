1f9b70e7c6aabd308afdf32a7e06c14949155d26
ViktoreNnamdi <nnamdyvictor@gmail.com>
2023-03-13 19:39:47 +0100

    added about page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ReviewList from "./component/ReviewList"
 import './index.css'
 import ReviewData from "./component/ReviewData"
 import About from './component/pages/About';
-// import Aboutlink from './component/Aboutlink';
+ import Aboutlink from './component/Aboutlink';
 
 function App() {
   const [review, setReview] = useState(ReviewData)
@@ -28,12 +28,12 @@ function App() {
       <Header/>
     <div className="container">
       <Routes>
-      <Route exact path='/' element={
+      <Route exact path='/review-app' element={
         <>
       <ReviewForm handleAdd={addReview}/>
       <ReviewStats review={review}/>
       <ReviewList review={review} revDelete={deleteReview}/>
-      {/* <Aboutlink/> */}
+       <Aboutlink/> 
        </>
       }
       ></Route>
